58f4748fd808ea6e193c7dee46c5417be97db62d
zhiwen <zhiwen@smartx.com>
2019-06-27 19:30:29 +0800

    make sync

--- tsfeatures/utils.py
+++ tsfeatures/utils.py
@@ -9,17 +9,13 @@ from .special import time_series_cid_ce
 # 度量时序间的距离
 
 def time_series_cid_distance(series1, series2):
-    """
-    a measurement of two time series.
-    paper:
-    http://www.cs.ucr.edu/~eamonn/Complexity-Invariant%20Distance%20Measure.pdf
-    """
+    # a distance measurement of two time series.
+    # paper:
+    # http://www.cs.ucr.edu/~eamonn/Complexity-Invariant%20Distance%20Measure.pdf
+    
 
     ce1 = time_series_cid_ce(series1)
     ce2 = time_series_cid_ce(series2)
-
-    series1 = np.asarray(series1)
-    series2 = np.asarray(series2)
     ds = series1 - series2
 
     d = np.sqrt(np.dot(ds, ds)) * max(ce1, ce2) / min(ce1, ce2)

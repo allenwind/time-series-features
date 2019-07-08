98d64190093718a2814ee81e8b911892c086fdf3
zhiwen <zhiwen@smartx.com>
2019-07-08 14:37:05 +0800

    update

--- tsfeatures/__init__.py
+++ tsfeatures/__init__.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+from .utils import find_time_series_max_periodic
 from .statistics import extract_time_series_statistics_features
-from .autocorrelation import (extract_time_series_autocorrelation_based_features, find_time_series_max_periodic)
+from .autocorrelation import extract_time_series_autocorrelation_based_features
 from .fitting import extract_time_series_fitting_features
 from .change import extract_time_series_change_features
 from .dispersion import extract_time_series_dispersion_features
@@ -16,7 +17,8 @@ from .index import (extract_time_series_forecast_features,
 __all__ = ["extract_time_series_statistics_features", "extract_time_series_autocorrelation_based_features", "extract_time_series_fitting_features",
            "extract_time_series_change_features", "extract_time_series_dispersion_features", "extract_time_series_peak_features", "compute_features_size",
            "extract_time_series_location_features", "extract_time_series_all_features", "extract_time_series_forecast_features", 
-           "extract_time_series_regression_features", "extract_time_series_anomaly_features", "extract_time_series_classification_features"]
+           "extract_time_series_regression_features", "extract_time_series_anomaly_features", "extract_time_series_classification_features", 
+           "find_time_series_max_periodic"]
 
 def compute_features_size(window_size=100):
     series = np.random.uniform(0.1, 10, window_size)

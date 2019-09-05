daf389051a06c1da9e550facf0ed1438171e4a71
zhiwen <zhiwen@smartx.com>
2019-09-05 12:29:56 +0800

    Add 3 entropy ensitmators

--- tsfeatures/info.py
+++ tsfeatures/info.py
@@ -1,44 +1,75 @@
 import numpy as np
 
 # 信息论或信号处理(小波)中的有关度量
+
 # entropy:
 # 1. approximate entropy
 # 2. binned entropy
 # 3. sample entropy
+# 这三个特征都可以用来度量复杂性, 因此可以用于分类任务.
 
 class time_series_approximate_entropy:
+
+    def __init__(self, m=2, r=3):
+        self.m = m
+        self.r = r
+
+    def __call__(self, series):
+        # 度量时间序列的波动的不可预测性
+        # 数值越小,序列的确定性越大
+        # wiki:
+        # https://en.wikipedia.org/wiki/Approximate_entropy
+
+        # 窗口太大,无法进行合理的切分
+        if series.size <= self.m+1:
+            return 0
+
+        r = self.r * np.std(series) # 过滤阈值
+        m = self.m # 窗口大小
+        return np.abs(self._phi(series, m, r) - self._phi(series, m+1, r))
+
+    def _phi(self, series, m, r):
+        # 转换为滑动窗口形式
+        X = np.array([series[i:i+m] for i in range(series.size-m+1)])
+        C = np.sum(np.max(np.abs(X[:, np.newaxis] - X[np.newaxis, :]), axis=2) <= r, axis=0) / (series.size-m+1)
+        return np.sum(np.log(C)) / (series.size-m+1)
+
+class time_series_binned_entropy:
+
+    # 直接进行简单的概率密度估计
+    # 根据熵的定义计算结果
+    
+    def __init__(self, bins=None):
+        self.bins = bins
+
+    def __call__(self, series):
+        probs, _ = np.histogram(series, bins=self.bins) / series.size
+        probs = probs[probs != 0] # 过滤 0, 否则取对数出现 np.inf
+        return -np.sum(probs * np.log(probs))
+
+class time_series_sample_entropy:
+
+    # 样本熵,度量时序复杂性的一种方式
+    # wiki:
+    # https://www.wikiwand.com/en/Sample_entropy
 
     def __init__(self, m, r):
         self.m = m
         self.r = r
 
     def __call__(self, series):
-        # 度量时间序列的波动的不可预测性
-        # wiki:
-        # https://en.wikipedia.org/wiki/Approximate_entropy
-        
-        n = len(series)
-        r *= np.std(x)
-        if n <= m+1:
+        if series.size < self.m+1:
             return 0
-        return np.abs(self._phi(self.m, self.r) - self._phi(self.m+1, self.r))
+        
+        r = self.r * np.std(series)
+        A = self._phi(series, self.m+1, r)
+        B = self._phi(series, self.m, r)
+        return -np.log(A/B)
 
-    def _phi(self, m, r):
-        N = x.size
-        x_re = np.array([x[i:i+m] for i in range(N - m + 1)])
-        C = np.sum(np.max(np.abs(x_re[:, np.newaxis] - x_re[np.newaxis, :]),
-                        axis=2) <= r, axis=0) / (N-m+1)
-        return np.sum(np.log(C)) / (N - m + 1.0)
-
-class time_series_binned_entropy:
-    
-    def __init__(self, max_bins):
-        self.max_bins = max_bins
-
-    def __call__(self, series):
-        hist, bin_edges = np.histogram(x, bins=self.max_bins)
-        probs = hist / x.size
-        return -np.sum(p * np.math.log(p) for p in probs if p != 0)
+    def _phi(self, series, m, r):
+        X = np.array([series[i:i+m] for i in range(series.size-m+1)])
+        E = np.sum(np.max(np.abs(X[:, np.newaxis] - X[np.newaxis, :]), axis=2) < r, axis=0)
+        return E
 
 def time_series_sample_entropy(series):
     x = np.array(x)
